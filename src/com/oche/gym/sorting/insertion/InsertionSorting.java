b3728a92330bb4a81809c1d658311e3638167bef
alex.chekanskyi <alex.chekanskyi@bonial.com>
2016-10-25 14:41:46 +0200

    Insertion Sorting implementation

--- src/com/oche/gym/sorting/insertion/InsertionSorting.java
+++ src/com/oche/gym/sorting/insertion/InsertionSorting.java
@@ -6,19 +6,35 @@ import java.util.Arrays;
  * Created by ache on 25.10.16.
  */
 public class InsertionSorting {
+
     public static void main(String[] args) {
-        int[] a = new int[] {2, 4, 6, 8, 3};
-        insertIntoSorted(a);
+        int[] abc = new int[]{10, 9, 8, 7, 5, 6, 4, 1, 2, 3, 3};
+        insertionSort(abc);
+        System.out.println(Arrays.toString(abc));
     }
 
-    public static void insertIntoSorted(int[] a) {
-        for(int i=a.length-1; i>1;i--) {
+    public static void insertionSort(int[] a) {
+        for(int i=1; i<a.length; i++){
+            insertIntoSortedWithStartPosition(a, i);
+        }
+    }
 
-            if(a[i] < a[i-1]) {
-                int temp = a[i-1];
-                a[i-1] = a[i];
-                a[i] = temp;
-                System.out.print(Arrays.toString(a));
+    /**
+     *
+     * @param a array which is already partially sorted
+     * @param position position of a first unsorted entry
+     */
+    public static void insertIntoSortedWithStartPosition(int[] a, int position) {
+        int unsortedVal = a[position];
+        for(int i=position; i>0;i--) {
+            if(a[i-1] > unsortedVal) {
+                a[i] = a[i-1];
+            } else {
+                a[i] = unsortedVal;
+                break;
+            }
+            if(i == 1) {
+                a[0] = unsortedVal;
             }
         }
     }
